f3c4e925b4892e89c1863362b9a8fb1be88a458a
Valentin Buchhold <buchhold@kit.edu>
2017-03-25 17:21:58 +0100

    Replaced RoutingCostAttribute with TravelCostAttribute.

---
+++ DataStructures/Graph/Attributes/TravelCostAttribute.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cassert>
+
+#include "DataStructures/Graph/Attributes/AbstractAttribute.h"
+#include "Tools/Constants.h"
+
+// An attribute associating a travel cost with each edge of a graph.
+class TravelCostAttribute : public AbstractAttribute<int> {
+ public:
+  static constexpr Type DEFAULT_VALUE = INFTY;         // The attribute's default value.
+  static constexpr const char* NAME   = "travel_cost"; // The attribute's unique name.
+
+  // Returns the travel cost of edge e.
+  const Type& travelCost(const int e) const {
+    assert(e >= 0); assert(e < values.size());
+    return values[e];
+  }
+
+  // Returns a reference to the travel cost of edge e.
+  Type& travelCost(const int e) {
+    assert(e >= 0); assert(e < values.size());
+    return values[e];
+  }
+};
