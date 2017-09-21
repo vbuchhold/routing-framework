16443c9b001b267e69ce9880c434c438c05c7770
Valentin Buchhold <buchhold@kit.edu>
2017-09-21 13:40:04 +0200

    Added the second part of the Visualization Framework: a facility for drawing geometric primitives.

---
+++ Visualization/Color.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+
+// A color in the RGB color space. Every color has an implicit alpha value of 1.0 or an explicit
+// one provided in the constructor. The color and alpha components can be specified by values in
+// the range 0.0-1.0 or 0-255.
+class Color {
+ public:
+  // Constructs a color with the specified red, green, blue and alpha values in the range 0-255.
+  constexpr Color(const int red, const int green, const int blue, const int alpha = 255)
+      : redValue(red), greenValue(green), blueValue(blue), alphaValue(alpha) {
+    assert(red >= 0); assert(red <= 255);
+    assert(green >= 0); assert(green <= 255);
+    assert(blue >= 0); assert(blue <= 255);
+    assert(alpha >= 0); assert(alpha <= 255);
+  }
+
+  // Constructs a color with the specified hexadecimal representation.
+  explicit constexpr Color(const uint32_t rgba, const bool hasAlpha = false)
+      : redValue(rgba >> 16),
+        greenValue(rgba >> 8),
+        blueValue(rgba),
+        alphaValue(hasAlpha ? rgba >> 24 : 255) {}
+
+  // Constructs a color with the specified red, green, blue and alpha values in the range 0.0-1.0.
+  constexpr Color(const double red, const double green, const double blue, const double alpha = 1.0)
+      : redValue(std::round(red * 255)),
+        greenValue(std::round(green * 255)),
+        blueValue(std::round(blue * 255)),
+        alphaValue(std::round(alpha * 255)) {
+    assert(red >= 0.0); assert(red <= 1.0);
+    assert(green >= 0.0); assert(green <= 1.0);
+    assert(blue >= 0.0); assert(blue <= 1.0);
+    assert(alpha >= 0.0); assert(alpha <= 1.0);
+  }
+
+  // Returns the red value in the range 0-255.
+  uint8_t red() const {
+    return redValue;
+  }
+
+  // Returns the green value in the range 0-255.
+  uint8_t green() const {
+    return greenValue;
+  }
+
+  // Returns the blue value in the range 0-255.
+  uint8_t blue() const {
+    return blueValue;
+  }
+
+  // Returns the alpha value in the range 0-255.
+  uint8_t alpha() const {
+    return alphaValue;
+  }
+
+  // Returns the red value in the range 0.0-1.0.
+  double normalizedRed() const {
+    return redValue / 255.0;
+  }
+
+  // Returns the green value in the range 0.0-1.0.
+  double normalizedGreen() const {
+    return greenValue / 255.0;
+  }
+
+  // Returns the blue value in the range 0.0-1.0.
+  double normalizedBlue() const {
+    return blueValue / 255.0;
+  }
+
+  // Returns the alpha value in the range 0.0-1.0.
+  double normalizedAlpha() const {
+    return alphaValue / 255.0;
+  }
+
+ private:
+  uint8_t redValue;
+  uint8_t greenValue;
+  uint8_t blueValue;
+  uint8_t alphaValue;
+};
+
+// Some predefined colors.
+constexpr Color KIT_GREEN = Color(0, 150, 130);
+constexpr Color KIT_BLUE = Color(70, 100, 170);
+constexpr Color KIT_BLACK = Color(0, 0, 0);
+constexpr Color KIT_PALEGREEN = Color(130, 190, 60);
+constexpr Color KIT_YELLOW = Color(250, 230, 20);
+constexpr Color KIT_ORANGE = Color(220, 160, 30);
+constexpr Color KIT_BROWN = Color(160, 130, 50);
+constexpr Color KIT_RED = Color(160, 30, 40);
+constexpr Color KIT_LILAC = Color(160, 0, 120);
+constexpr Color KIT_CYANBLUE = Color(80, 170, 230);
+
+// Some predefined color schemes.
+constexpr Color YL_OR_RD_9CLASS[] = {
+  Color(0xffffcc), Color(0xffeda0), Color(0xfed976),
+  Color(0xfeb24c), Color(0xfd8d3c), Color(0xfc4e2a),
+  Color(0xe31a1c), Color(0xbd0026), Color(0x800026) };
---
+++ Visualization/PrimitiveDrawer.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <cassert>
+
+#include <cairo/cairo.h>
+
+#include "DataStructures/Geometry/Point.h"
+#include "DataStructures/Geometry/Polygon.h"
+#include "Visualization/Color.h"
+#include "Visualization/Graphic.h"
+
+// Some predefined line width values.
+struct LineWidth {
+  static constexpr double ULTRA_THIN = 0.1;
+  static constexpr double VERY_THIN = 0.2;
+  static constexpr double THIN = 0.4;
+  static constexpr double SEMITHICK = 0.6;
+  static constexpr double THICK = 0.8;
+  static constexpr double VERY_THICK = 1.2;
+  static constexpr double ULTRA_THICK = 1.6;
+};
+
+// A facility for drawing geometric primitives like lines and polygons on a graphic.
+class PrimitiveDrawer {
+ public:
+  // Constructs a drawer for geometric primitives.
+  PrimitiveDrawer(Graphic* const graphic = nullptr,
+                  const Color& color = KIT_BLACK, const double width = LineWidth::THIN)
+      : currentGraphic(graphic), currentColor(color), currentLineWidth(width) {
+    initCairoContext();
+  }
+
+  // Initializes the cairo drawing context of the current graphic.
+  void initCairoContext() {
+    if (currentGraphic != nullptr && currentGraphic->getCairoContext() != nullptr) {
+      setColor(currentColor);
+      setLineWidth(currentLineWidth);
+      cairo_set_line_cap(currentGraphic->getCairoContext(), CAIRO_LINE_CAP_ROUND);
+    }
+  }
+
+  // Sets the graphic to be drawn on.
+  void setGraphic(Graphic& graphic) {
+    currentGraphic = &graphic;
+    initCairoContext();
+  }
+
+  // Sets the color to be used for drawing.
+  void setColor(const Color& color) {
+    currentColor = color;
+    if (currentGraphic != nullptr && currentGraphic->getCairoContext() != nullptr)
+      cairo_set_source_rgba(
+          currentGraphic->getCairoContext(),
+          currentColor.normalizedRed(), currentColor.normalizedGreen(),
+          currentColor.normalizedBlue(), currentColor.normalizedAlpha());
+  }
+
+  // Sets the line width (in points or pixels) to be used for drawing.
+  void setLineWidth(const double width) {
+    currentLineWidth = width;
+    if (currentGraphic != nullptr && currentGraphic->getCairoContext() != nullptr) {
+      double x = currentLineWidth, y = 0;
+      cairo_device_to_user_distance(currentGraphic->getCairoContext(), &x, &y);
+      cairo_set_line_width(currentGraphic->getCairoContext(), x);
+    }
+  }
+
+  // Draws a line between the specified points, possibly setting the current color and line width.
+  void drawLine(const Point& from, const Point& to) {
+    assert(currentGraphic != nullptr); assert(currentGraphic->isOpen());
+    cairo_t* const ctx = currentGraphic->getCairoContext();
+    cairo_move_to(ctx, from.getX(), from.getY());
+    cairo_line_to(ctx, to.getX(), to.getY());
+    cairo_stroke(ctx);
+  }
+  void drawLine(const Point& from, const Point& to, const Color& color) {
+    setColor(color);
+    drawLine(from, to);
+  }
+  void drawLine(const Point& from, const Point& to, const double width) {
+    setLineWidth(width);
+    drawLine(from, to);
+  }
+  void drawLine(const Point& from, const Point& to, const Color& color, const double width) {
+    setColor(color);
+    setLineWidth(width);
+    drawLine(from, to);
+  }
+
+  // Draws the specified polygon, possibly setting the current color and line width.
+  void drawPolygon(const Polygon& polygon) {
+    assert(currentGraphic != nullptr); assert(currentGraphic->isOpen());
+    if (polygon.empty())
+      return;
+    cairo_t* const ctx = currentGraphic->getCairoContext();
+    cairo_move_to(ctx, polygon[0].getX(), polygon[0].getY());
+    for (int i = 1; i < polygon.size(); ++i)
+      cairo_line_to(ctx, polygon[i].getX(), polygon[i].getY());
+    cairo_close_path(ctx);
+    cairo_stroke(ctx);
+  }
+  void drawPolygon(const Polygon& polygon, const Color& color) {
+    setColor(color);
+    drawPolygon(polygon);
+  }
+  void drawPolygon(const Polygon& polygon, const double width) {
+    setLineWidth(width);
+    drawPolygon(polygon);
+  }
+  void drawPolygon(const Polygon& polygon, const Color& color, const double width) {
+    setColor(color);
+    setLineWidth(width);
+    drawPolygon(polygon);
+  }
+
+ private:
+  Graphic* currentGraphic; // The graphic to be drawn on.
+  Color currentColor;      // The color currently used for drawing.
+  double currentLineWidth; // The line width currently used for drawing.
+};
