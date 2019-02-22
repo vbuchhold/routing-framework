4237150006a1a1459d877df8aaf32b9bc8d73204
Valentin Buchhold <buchhold@kit.edu>
2019-02-22 04:47:09 +0100

    Updated the parameters for the BPR travel cost function.

--- Algorithms/TrafficAssignment/TravelCostFunctions/BprFunction.h
+++ Algorithms/TrafficAssignment/TravelCostFunctions/BprFunction.h
@@ -12,19 +12,19 @@ class BprFunction {
   // Returns the travel time on edge e, given the flow x on e.
   double operator()(const int e, const double x) const {
     const double tmp = x / graph.capacity(e);
-    return graph.travelTime(e) * (1 + 0.15 * tmp * tmp * tmp * tmp);
+    return graph.travelTime(e) * (1 + 1 * tmp * tmp);
   }
 
   // Returns the derivative of e's travel cost function at x.
   double derivative(const int e, const double x) const {
     const double tmp = x / graph.capacity(e);
-    return graph.travelTime(e) * 0.15 * 4 * tmp * tmp * tmp / graph.capacity(e);
+    return graph.travelTime(e) * 1 * 2 * tmp / graph.capacity(e);
   }
 
   // Returns the antiderivative of e's travel cost function at x.
   double antiderivative(const int e, const double x) const {
     const double tmp = x / graph.capacity(e);
-    return graph.travelTime(e) * (x + 0.15 * x * tmp * tmp * tmp * tmp / (4 + 1));
+    return graph.travelTime(e) * (x + 1 * x * tmp * tmp / (2 + 1));
   }
 
   // Returns the integral of e's travel cost function from 0 to b.
@@ -36,14 +36,14 @@ class BprFunction {
   Vec4d operator()(const int e, const Vec4d& x) const {
     Vec4d time = to_double(Vec4i().load(&graph.travelTime(e)));
     Vec4d capacity = to_double(Vec4i().load(&graph.capacity(e)));
-    return time * (1 + 0.15 * pow_const(x / capacity, 4));
+    return time * (1 + 1 * pow_const(x / capacity, 2));
   }
 
   // Returns the derivative of e's travel cost function at x.
   Vec4d derivative(const int e, const Vec4d& x) const {
     Vec4d time = to_double(Vec4i().load(&graph.travelTime(e)));
     Vec4d capacity = to_double(Vec4i().load(&graph.capacity(e)));
-    return time * 0.15 * 4 * pow_const(x / capacity, 4 - 1) / capacity;
+    return time * 1 * 2 * pow_const(x / capacity, 2 - 1) / capacity;
   }
 
  private:
