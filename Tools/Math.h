2b94ec97f7b680cf5a07d1753f93134ad8a417fe
Valentin Buchhold <buchhold@kit.edu>
2019-06-21 17:20:28 +0200

    Added functions for rounding to powers of two.

--- Tools/Math.h
+++ Tools/Math.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <cassert>
+#include <limits>
+
+#include "Tools/Bitwise.h"
+
 // The ratio of the circumference of a circle to its diameter.
 constexpr double PI = 3.14159265358979323846l;
 
@@ -11,6 +16,20 @@ inline constexpr double toRadians(const double angdeg) {
 // Converts an angle measured in radians to an approximately equivalent angle measured in degrees.
 inline constexpr double toDegrees(const double angrad) {
   return 180 / PI * angrad;
+}
+
+// Returns the smallest power of two not less than x.
+template <typename UIntT>
+inline UIntT roundUpToPowerOfTwo(const UIntT x) {
+  assert(x > 0); assert(x <= UIntT{1} << (std::numeric_limits<UIntT>::digits - 1));
+  return UIntT{1} << (highestOneBit(x - 1) + 1);
+}
+
+// Returns the largest power of two not greater than x.
+template <typename UIntT>
+inline UIntT roundDownToPowerOfTwo(const UIntT x) {
+  assert(x > 0);
+  return UIntT{1} << highestOneBit(x);
 }
 
 // Sets min to the smaller and max to the greater of a and b.
