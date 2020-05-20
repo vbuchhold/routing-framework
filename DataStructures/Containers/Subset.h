9e5b8b7f33a003fae6b8816a5376cdb3b2868de2
Valentin Buchhold <buchhold@kit.edu>
2020-05-20 20:55:41 +0200

    Added support for iterating through and clearing a subset in optimal time.

--- DataStructures/Containers/Subset.h
+++ DataStructures/Containers/Subset.h
@@ -1,30 +1,70 @@
 #pragma once
 
-#include "DataStructures/Containers/BitVector.h"
+#include <cassert>
+#include <cstdint>
+#include <vector>
 
-// The class represents a subset of a finite set of size n. It is implemented as a bit vector of
-// size n. Each bit indicates whether an element of the finite set is in the subset or not.
+#include "Tools/Constants.h"
+
+// This class represents a subset of a finite set of size n. Inserting elements, removing elements,
+// and testing elements for membership take constant time. Iterating through and clearing a subset
+// of size k both take time O(k).
 class Subset {
  public:
-  // Constructs an (empty or complete) subset of a finite set of the specified size.
-  explicit Subset(const int size, const bool complete = false)
-      : isElementContained(size, complete) {}
-
-  // Returns true if the subset contains the element with the specified index.
-  bool contains(const int idx) const {
-    return isElementContained[idx];
+  // Constructs an empty subset of a finite set of the specified size.
+  explicit Subset(const int size) : elementsToIndices(size, INVALID_INDEX) {
+    elements.reserve(size);
   }
 
-  // Inserts the element with the specified index into the subset.
-  void insert(const int idx) {
-    isElementContained[idx] = true;
+  // Returns an iterator referring to the first element in the subset.
+  std::vector<int32_t>::const_iterator begin() const noexcept {
+    return elements.begin();
   }
 
-  // Removes the element with the specified index idx from the subset.
-  void remove(const int idx) {
-    isElementContained[idx] = false;
+  // Returns the past-the-end iterator for the subset.
+  std::vector<int32_t>::const_iterator end() const noexcept {
+    return elements.end();
+  }
+
+  // Returns the number of elements in the subset.
+  int size() const noexcept {
+	  return elements.size();
+  }
+
+  // Inserts the specified element into the subset. Invalidates only the past-the-end iterator.
+  bool insert(const int element) {
+    if (contains(element))
+      return false;
+    elementsToIndices[element] = elements.size();
+    elements.push_back(element);
+    return true;
+  }
+
+  // Removes the specified element from the subset. May invalidate all iterators.
+  bool remove(const int element) {
+    if (!contains(element))
+      return false;
+    elements[elementsToIndices[element]] = elements.back();
+    elementsToIndices[elements.back()] = elementsToIndices[element];
+    elements.pop_back();
+    elementsToIndices[element] = INVALID_INDEX;
+    return true;
+  }
+
+  // Removes all elements in the subset. May invalidate all iterators.
+  void clear() {
+    for (const auto element : elements)
+      elementsToIndices[element] = INVALID_INDEX;
+    elements.clear();
+  }
+
+  // Returns true if the subset contains the specified element.
+  bool contains(const int element) const {
+    assert(element >= 0); assert(element < elementsToIndices.size());
+    return elementsToIndices[element] != INVALID_INDEX;
   }
 
  private:
-  BitVector isElementContained; // Each bit indicates whether an element is in the subset or not.
+  std::vector<int32_t> elements;          // The elements contained in the subset.
+  std::vector<int32_t> elementsToIndices; // The index in the element array of each element.
 };
